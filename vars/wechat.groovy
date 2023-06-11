8f9e45272bd4af057d227d3a0e4694917ad8ec58
ninggf <windywany@163.com>
2023-06-11 09:38:40 +0800

    显示最近三次提交

--- vars/wechat.groovy
+++ vars/wechat.groovy
@@ -33,7 +33,7 @@ def call(Map args) {
 
             def git_log = 'git log --pretty=format:"> - %cn@%ad - %s" --date=format:"%Y-%m-%d %H:%M:%S" --since="' + last_start_date + '" '
             env.commitChangeset = sh(returnStdout: true, script: "${git_log}").trim()
-            if (!env.commitChangeset) {
+            if (env.commitChangeset == '') {
                 git_log = 'git log --pretty=format:"> - %cn@%ad - %s" --date=format:"%Y-%m-%d %H:%M:%S" -n 3'
                 env.commitChangeset = sh(returnStdout: true, script: "${git_log}").trim()
             }
