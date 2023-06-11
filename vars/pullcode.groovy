9f9d8e900078cdee590a83213d6ddd49f36bdd3e
ninggf <windywany@163.com>
2023-06-11 09:42:22 +0800

    显示最近三次提交

--- vars/pullcode.groovy
+++ vars/pullcode.groovy
@@ -22,7 +22,11 @@ def call(Map<String, String> args) {
 
     def git_log = 'git log --pretty=format:"> - %cn@%ad - %s" --date=format:"%Y-%m-%d %H:%M:%S" --since="' + last_start_date + '" '
     env.commitChangeset = sh(returnStdout: true, script: "${git_log}").trim()
-
+    echo "changelog: '${env.commitChangeset}'"
+    if (env.commitChangeset == '') {
+        git_log = 'git log --pretty=format:"> - %cn@%ad - %s" --date=format:"%Y-%m-%d %H:%M:%S" -n 3'
+        env.commitChangeset = sh(returnStdout: true, script: "${git_log}").trim()
+    }
     env.gitCommit = sh(returnStdout: true, script: "git rev-parse HEAD").trim()
 }
 
